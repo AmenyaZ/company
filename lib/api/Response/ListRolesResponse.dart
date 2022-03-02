f7beb538818f4d3729d70537a21f9db7d0009ddd
AmenyaZ <zamenya98@gmail.com>
2022-03-02 12:23:43 +0300

    Add response file of Role List API call

---
+++ lib/api/Response/ListRolesResponse.dart
@@ -0,0 +1,38 @@
+import 'ListRoles/Role.dart';
+import 'dart:convert';
+
+ListRolesResponse listRolesResponseFromJson(String str) => ListRolesResponse.fromJson(json.decode(str));
+String listRolesResponseToJson(ListRolesResponse data) => json.encode(data.toJson());
+class ListRolesResponse {
+  ListRolesResponse({
+      List<Role>? role, 
+      String? message,}){
+    _role = role;
+    _message = message;
+}
+
+  ListRolesResponse.fromJson(dynamic json) {
+    if (json['role'] != null) {
+      _role = [];
+      json['role'].forEach((v) {
+        _role?.add(Role.fromJson(v));
+      });
+    }
+    _message = json['message'];
+  }
+  List<Role>? _role;
+  String? _message;
+
+  List<Role>? get role => _role;
+  String? get message => _message;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    if (_role != null) {
+      map['role'] = _role?.map((v) => v.toJson()).toList();
+    }
+    map['message'] = _message;
+    return map;
+  }
+
+}
